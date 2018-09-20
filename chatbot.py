87d46f6ff0ba78c54ae6a2dae62b0fdd6c816b21
RyanKopec <Rpec91@gmail.com>
2018-09-19 21:36:26 -0400

    Adding import for speech recognition and move lines file

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,8 @@ import numpy as np
 import tensorflow as tf
 import re
 import time
+import speech_recognition as sr
+
 
 lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
 conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
